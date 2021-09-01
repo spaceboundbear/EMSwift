08777c8cf0c92efe235093b1639e3848a3aaaa11
Ben Townsend <RBT000011@gmail.com>
2021-09-01 18:09:57 -0500

    added forms section, CSS and definitions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+var firstNameInput = document.querySelector("#first_name")
+var lastNameInput= document.querySelector("#last_name")
+var symptomsInput = document.querySelector("#symptoms")
+var addressInput = document.querySelector("#address")
+var responderInput = document.querySelector("#responder")
+var saveButton = document.querySelector(".save")
+
+
 $(document).ready(function () {
   $('.sidenav').sidenav();
   $('.fixed-action-btn').floatingActionButton();
