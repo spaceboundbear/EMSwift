10647bcbd81e8d632447807bf53811773fda69d9
spaceboundbear <mf.michaelfischer@gmail.com>
2021-09-13 02:02:45 -0500

    cleaning code

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,10 @@ var address = $('#address-input');
 var idNum = $('#response-id');
 var gender = $('#gender');
 var bp = $('#blood-pressure');
-var symptomsNumbers = [];
 var notes = $('#notes');
 var submitForm = $('.form-submit');
 var patientInfo = JSON.parse(localStorage.getItem('patientinfo')) || [];
+var symptomsInfo = JSON.parse(localStorage.getItem('symptoms'));
 var patient = '';
 
 submitForm.on('click', function submitInfo(patient) {
@@ -78,105 +78,47 @@ function addToList(c) {
   $('.add-test').append(start);
 }
 
-/*   if (patientInfo == null) {
-    patientInfo = [];
-    patientInfo.push(patient);
-    localStorage.setItem('patientinfo', JSON.stringify(patientInfo));
-    addToList(patient);
-  }
+/*------------------- medical api -------------------- */
 
-  for (var i = 0; i < patientInfo.length; i++) {
-    addToList(patientInfo[i]);
-
-    function addToList(c) {
-      var start = $('<div> test test');
-      $(start).attr(
-        'class',
-        'uk-card uk-card-default uk-card-body uk-width-1-1@m start'
-      );
-      $('.add-test').after(start);
-
-      var a = $('<h3>' + c.firstName + ' ' + c.lastName + '</h3>');
-      $(a).attr('class', 'uk-card-title nameEl test-content');
-      $('.start').append(a);
-
-      var b = $('</p> TEST TEST SET SE TSE ET ETS TE S TES</p>');
-      $(b).attr('class', 'p');
-      $('.nameEl').after(b);
-
-      var c = $('</div>');
-      $('.p').after(c);
-    }
-  }
-
-/*    $(".nameEl").text(
-      patientInfo[i].firstName.toUpperCase() +
-        " " +
-        patientInfo[i].lastName.toUpperCase()
-    );
-    $(".address-text").text("Address: " + patientInfo[i].address);
-    $(".age-text").text("Age: " + patientInfo[i].birthYear);
-    $(".bp-text").text("Blood Pressure: " + patientInfo[i].bloodPressure);
-    $(".notes-text").text("Notes: " + patientInfo[i].notes);
-    $(".id-text").text("Responder ID#: " + patientInfo[i].responder);
-    $(".gender-val").text("Gender: " + patientInfo[i].gender);
-  }
-
-/*<ul uk-accordion class="accordion-start">
-        <li class="uk-open open-test">
-          <a class="uk-accordion-title nameEl test-content" href="#"></a>
-          <div class="uk-accordion-content test">
-            <div
-              class="
-                name-test
-                uk-card uk-card-default uk-card-body uk-width-1-1@s
-              "
-            >
-              <h5 class="uk-card-title">Info:</h5>
-              <p class="address-text"></p>
-              <p class="age-text"></p>
-              <p class="bp-text"></p>
-              <p class="notes-text"></p>
-              <p class="id-text"></p>
-              <p class="gender-val"></p>
-              <a class="uk-button uk-button-danger uk-align-top"
-                >Delete Patient</a
-              >
-            </div>
-          </div>
-        </li>
-      </ul>
-
-//var symptoms =
-//'https://sandbox-healthservice.priaid.ch/symptoms?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Im1mLm1pY2hhZWxmaXNjaGVyQGdtYWlsLmNvbSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiOTYyOCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvdmVyc2lvbiI6IjIwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGltaXQiOiI5OTk5OTk5OTkiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXAiOiJQcmVtaXVtIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAyMS0wOC0zMSIsImlzcyI6Imh0dHBzOi8vc2FuZGJveC1hdXRoc2VydmljZS5wcmlhaWQuY2giLCJhdWQiOiJodHRwczovL2hlYWx0aHNlcnZpY2UucHJpYWlkLmNoIiwiZXhwIjoxNjMxMjM0ODI3LCJuYmYiOjE2MzEyMjc2Mjd9.qTYJ1YWtwdhly_qdprtPPkkRQ3Bx7nY_NkeI2UMVa9I&format=json&language=en-gb';
-
-/* var diagnosis =
-  'https://sandbox-healthservice.priaid.ch/diagnosis?symptoms=' +
-  //symptomsNumbers +
-  '&gender=Male' +
-  //gender +
-  '&year_of_birth=1983' +
-  // birthYear +
-  '&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Im1mLm1pY2hhZWxmaXNjaGVyQGdtYWlsLmNvbSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiOTYyOCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvdmVyc2lvbiI6IjIwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGltaXQiOiI5OTk5OTk5OTkiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXAiOiJQcmVtaXVtIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAyMS0wOC0zMSIsImlzcyI6Imh0dHBzOi8vc2FuZGJveC1hdXRoc2VydmljZS5wcmlhaWQuY2giLCJhdWQiOiJodHRwczovL2hlYWx0aHNlcnZpY2UucHJpYWlkLmNoIiwiZXhwIjoxNjMxMjM1MTIzLCJuYmYiOjE2MzEyMjc5MjN9.Zkr14yldTVoyKfLdt8cWe7qzXueW2-mFgOI3eF3Cs3Q&format=json&language=en-gb';
-
-  */
-
-//console.log(symptoms);
-//console.log(diagnosis);
-
-/* fetch(symptoms)
-  .then(function (response) {
+fetch(
+  'https://sandbox-healthservice.priaid.ch/symptoms?language=en-gb&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Im1mLm1pY2hhZWxmaXNjaGVyQGdtYWlsLmNvbSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiOTYyOCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvdmVyc2lvbiI6IjIwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGltaXQiOiI5OTk5OTk5OTkiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXAiOiJQcmVtaXVtIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAyMS0wOC0zMSIsImlzcyI6Imh0dHBzOi8vc2FuZGJveC1hdXRoc2VydmljZS5wcmlhaWQuY2giLCJhdWQiOiJodHRwczovL2hlYWx0aHNlcnZpY2UucHJpYWlkLmNoIiwiZXhwIjoxNjMxNDEwODAzLCJuYmYiOjE2MzE0MDM2MDN9.TFmm_adga7777JuuBuqo6PeGReIykD_g05S9JiPcf98'
+)
+  .then((response) => {
     return response.json();
   })
-  .then(function (data) {
-    console.log(data);
+  .then((symptoms) => {
+    localStorage.setItem('symptoms', JSON.stringify(symptoms));
   });
 
-/*fetch(diagnosis)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
-  */
+// auto complete function
+
+function saveArr() {
+  var obj = {
+      metadata: {
+        products: symptomsInfo,
+      },
+    },
+    name = obj.metadata.products
+      .map(function (product) {
+        return product.Name;
+      })
+      .join(', ');
+
+  localStorage.setItem('nameString', name);
+  console.log(name);
+}
+
+$(document).ready(function () {
+  var name = localStorage.getItem('nameString');
+
+  for (var i = 0; i < name.length; i++) {
+    // Creating an array with country names and load them into local storage
+    // Adding autocmplete functionality to a textfield with data from local storage
+    $('#autocomplete').autocomplete({
+      source: name,
+      minLength: 1,
+      delay: 0,
+    });
+  }
+  console.log(name);
+});
