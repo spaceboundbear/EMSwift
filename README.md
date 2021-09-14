d8abbdc586e99ad1da2accb8bb49a8d82539e657
spaceboundbear <mf.michaelfischer@gmail.com>
2021-09-14 15:29:03 -0500

    last minute changes to README and mapscript

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# test-project
+EMSwift
+
+This application is a simple way for EMS and first responders to keep track of patients under their care. A user can store patient information, vitals, symptoms, view past patients, and retrieve local nearby hospitals with traffic updates to make an accurate decision on where to transport patients.
+
+This app uses MapQuest and Endless Medical APIs
+This app also keeps track of patients using local storage
+
+Note: This is simply a proof of concept and does not comply with HIPAA standards and regulations; be aware that this is only for testing and not for actual use in the real world (for now).
+
+Link to app: EMSwift.com
+link to repository: https://github.com/spaceboundbear/Project1-UTSA-MF-DB-BT
+
+Image of Application:
--- assets/js/mapScript.js
+++ assets/js/mapScript.js
@@ -1,84 +1,61 @@
 window.onload = function () {
   let ps = placeSearch({
-    key: 'lYrP4vF3Uk5zgTiGGuEzQGwGIVDGuy24',
-    container: document.querySelector('#search-input'),
+    key: "XSsrppciu4Wo2TcLVwPDASSl7fjw2a7z",
+    container: document.querySelector("#search-input"),
     useDeviceLocation: true,
-    collection: ['poi', 'address', 'franchise'],
+    collection: ["category", "franchise"],
   });
 
-  L.mapquest.key = 'lYrP4vF3Uk5zgTiGGuEzQGwGIVDGuy24';
 
-  var map = L.mapquest.map('map', {
+  L.mapquest.key = "XSsrppciu4Wo2TcLVwPDASSl7fjw2a7z";
+
+  var map = L.mapquest.map("map", {
     center: [29.4241, -98.4936],
-    layers: L.mapquest.tileLayer('map'),
+    layers: L.mapquest.tileLayer("map"),
     zoom: 11,
   });
 
   L.mapquest.control().addTo(map);
 
-  let markers = [];
+  let searchLayer = null;
 
-  ps.on('change', (e) => {
-    markers.forEach(function (marker, markerIndex) {
-      if (markerIndex === e.resultIndex) {
-        markers = [marker];
-        marker.setOpacity(1);
-        map.setView(e.result.latlng, 11);
-      } else {
-        removeMarker(marker);
-      }
-    });
+  ps.on("change", (e) => {
+    if (e.result.type === "franchise" || e.result.type === "category") {
+      let search = L.mapquest.search();
+      search.place(
+        {
+          category: `sic:${e.result.sic[0]}`,
+          sort: "relevance",
+          bbox: map.getBounds(),
+          pageSize: 20,
+        },
+        handleSearch
+      );
+    }
   });
 
-  ps.on('results', (e) => {
-    markers.forEach(removeMarker);
-    markers = [];
-
-    if (e.results.length === 0) {
-      map.setView(new L.LatLng(0, 0), 2);
-      return;
+  function handleSearch(err, data) {
+    if (searchLayer) {
+      map.removeLayer(searchLayer);
     }
 
-    e.results.forEach(addMarker);
-    findBestZoom();
-  });
-
-  ps.on('cursorchanged', (e) => {
-    markers.forEach(function (marker, markerIndex) {
-      if (markerIndex === e.resultIndex) {
-        marker.setOpacity(1);
-        marker.setZIndexOffset(1000);
-      } else {
-        marker.setZIndexOffset(0);
-        marker.setOpacity(0.5);
-      }
+    searchLayer = L.mapquest.searchLayer({
+      searchResponse: data,
     });
+    map.addLayer(searchLayer);
+  }
+
+  ps.on("clear", () => {
+    if (searchLayer) {
+      map.removeLayer(searchLayer);
+    }
+    searchLayer = null;
+    map.setView(new L.LatLng(29.4241, -98.4936), 5);
   });
 
-  ps.on('clear', () => {
-    console.log('cleared');
-    map.setView(new L.LatLng(0, 0), 2);
-    markers.forEach(removeMarker);
-  });
-
-  ps.on('error', (e) => {
+  ps.on("error", (e) => {
     console.log(e);
   });
-
-  function addMarker(result) {
-    let marker = L.marker(result.latlng, { opacity: 0.4 });
-    marker.addTo(map);
-    markers.push(marker);
-  }
-
-  function removeMarker(marker) {
-    map.removeLayer(marker);
-  }
-
-  function findBestZoom() {
-    let featureGroup = L.featureGroup(markers);
-    map.fitBounds(featureGroup.getBounds().pad(0.5), { animate: false });
-  }
 };
 
 //  L.mapquest.key = 'XSsrppciu4Wo2TcLVwPDASSl7fjw2a7z';
